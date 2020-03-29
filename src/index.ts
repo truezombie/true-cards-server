80a8de916c590d3e9b1560cb927495597da0be68
Oleg Ovcharenko <oleshka.ovcharenko@gmail.com>
2020-03-29 11:05:56 +0300

    feat: cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-const sum = (a: number, b: number) => {
-  return a + b;
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
 };
 
-const concat = (one: string, two: string) => {
-  return one + two;
-};
+const server = new ApolloServer({ typeDefs, resolvers });
 
-export { sum, concat };
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
---
+++ src/schemas/schema.ts
@@ -0,0 +1,7 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  # Your schema will go here
+`;
+
+module.exports = typeDefs;
