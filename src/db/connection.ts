2c14d399d72bc0885c9743c2f964f5d287ce4743
Oleg Ovcharenko <oleshka.ovcharenko@gmail.com>
2020-09-01 10:22:21 +0300

    feat: changes

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 import config from '../utils/config';
 
-console.log(config, process.env);
 const dbPath = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}?retryWrites=true&w=majority`;
 
 const connectToMongoDb = async () => {
